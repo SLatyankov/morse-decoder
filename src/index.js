ac44ca6f86af6d10fa86504b52706890e3695e00
SLatyankov <Slatiankov@yandex.ru>
2020-03-21 14:12:12 +0400

    апр

--- src/index.js
+++ src/index.js
@@ -38,9 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+var arrExpr = expr.split(''); 
+var arr = new Array;
+for (let i = 0; i <= arrExpr.length + 3000; i++) {
+    var x = arrExpr.splice(0, 10);
+    
+    if (x[0] === '*') {
+        arr[i] = ' ';
+    } else {
+        let y;
+        let z = '';
+        for (let j = 0; j < 5; j++) {
+            y = x.splice(0, 2);                  
+            if (y[0] == 1 && y[1] == 0) {
+                z += '.';
+            } else if (y[0] == 1 && y[1] == 1) {
+                z += '-';
+            }
+        }
+        arr[i] = MORSE_TABLE[z];
+    }
+}
+var str = arr.join('');
+
+return str;
 }
 
 module.exports = {
     decode
 }
+
